a37602ba423d4e1a8a98f30f7e1995a4c58d5ea1
Jeremy Moseley <jeremymoseley@me.com>
2021-03-08 15:45:20 -0800

    Center align for lg+ screens

--- src/Bracket.tsx
+++ src/Bracket.tsx
@@ -1,23 +1,34 @@
-import { makeStyles } from '@material-ui/core/styles'
+import clsx from 'clsx'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import Box from '@material-ui/core/Box'
 import Round from './Round'
 
 const useStyles = makeStyles(() => ({
   bracket: {
-    justifyContent: 'center',
+    justifyContent: 'left',
     marginTop: '100px',
     '& > div': {
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'space-around'
     }
+  },
+  center: {
+    justifyContent: 'center'
   }
 }))
 
 const Bracket = () => {
   const classes = useStyles()
+  const theme = useTheme()
+  const centerBrackets = useMediaQuery(theme.breakpoints.up('lg'))
   return (
-    <Box display='flex' className={classes.bracket}>
+    <Box
+      display='flex'
+      className={clsx(classes.bracket, {
+        [classes.center]: centerBrackets
+      })}>
       <Round round='WildCard' conference='afc' />
       <Round round='Divisional' conference='afc' />
       <Round round='Conference' conference='afc' />
